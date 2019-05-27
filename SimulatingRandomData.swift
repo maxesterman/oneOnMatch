677a9c07fc597afe7190ad926882d411e61a006a
Max Sterman <maxsterman@Maxs-MacBook-Pro.local>
2019-05-27 12:34:08 -0400

    adding first File

---
+++ SimulatingRandomData.swift
@@ -0,0 +1,26 @@
+//
+//  SimulatingRandomData.swift
+//  oneOnMatch
+//
+//  Created by Max Sterman on 12/2/16.
+//  Copyright © 2016 Max Sterman. All rights reserved.
+//
+
+import UIKit
+
+class SimulatingRandomData: UITableViewCell {
+    
+    let firstNames = [String]=["Adam", "Craig", "Sarah", "Sara", "Sam", "Bob", "Bill", "John", "Jacob", "Ryan", "Matthew", "Thomas", "Tom", "Jill", "Jared", "Jeff", "Sean", "Ray", "Raymond", "Brad", "Max", "Maxwell", "Frank", "Steve", "Kyle", "Cole", "Jorge", "Janice", "Will", "William", "Dick", "Richard", "Alex", "Alexandra", "Alexander", "Molly", "Steven", "Stephen", "Xenu", "Zachary", "Naphtali", "Antonio", "Kylie", "Kylee", "Nicole", "Nichole", "Manlio", "Jonathan", "Joseph", "Jar"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
